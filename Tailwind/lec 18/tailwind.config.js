f2fef8245bddcd844f996b854167e75975b3b1df
Muhammad Shakeeb Raza <shakeebraza90@gmail.com>
2024-11-01 08:55:30 -0700

    fix:"media query and custom css is there file "

--- Tailwind/lec 18/tailwind.config.js
+++ Tailwind/lec 18/tailwind.config.js
@@ -2,25 +2,29 @@
 module.exports = {
   content: ['*'],
   theme: {
-    extend: {
-      fontSize:{
-        "10xl":"20rem"
-      },
-      colors:{
-        red:{
-          '88':'#FFC0CB'
+    colors:{
+        blue: {
+            100:'#1fb6ff',
+            200:'#1fb6ff',
+            390:'#1fb6ff',
+            600:'#1fb6ff',
         },
-        "pink":'#FFC0CB',
+        'purple': '#7e5bef',
+        'pink': '#ff49db',
+        'orange': '#ff7849',
+        'green': '#13ce66',
+        'yellow': '#ffc82c',
+        'gray-dark': '#273444',
+        'gray': '#8492a6',
+        'gray-light': '#d3dce6',
       },
-      fontFamily: {
-        sans: ['Graphik', 'sans-serif'],
-        serif: ['Merriweather', 'serif'],
-      },
-      borderColor:{
-        "mygreen":"#00ff00"
-      }
+    extend: {
+        screens: {
+            'shakeeb': {'min': '640px', 'max': '767px'},
+        },
     },
-  },
+    },
+
   plugins: [],
 }
 
