8429127f12bf13677d8b10d87ff44e7fa24d2c37
Muhammad Shakeeb Raza <shakeebraza90@gmail.com>
2024-11-14 04:07:47 -0800

    fix:"Adding Responsiveness & Deploy on Netlify"

--- Tailwind/project-5/tailwind.config.js
+++ Tailwind/project-5/tailwind.config.js
@@ -4,9 +4,9 @@ module.exports = {
   theme: {
 
     extend: {
-      colors:{
-        nilla:'#00baf2',
-        darnila:'#002970',
+      colors :{
+        nilla: "#00baf2",
+        darkNilla: '#002970',
         lightColor:'#f5f5f5',
         lightNilla:'#00afe3',
         lightDarkNilla:'#0f4a8a',
@@ -15,13 +15,13 @@ module.exports = {
         line:'#cccccc',
         fontColor:'#505050',
         smallFontColor:'#707086'
-      }
     },
     screens : {
-      'verySmall' : {'max' : "399px"},
-      'small' : {'max' :"550px"},
-      'medium' :{'max' :"768px"},
-      'large': {'max':'1070px'},
+        'verySmall' : {'max' : "399px"},
+        'small' : {'max' :"550px"},
+        'medium' :{'max' :"768px"},
+        'large': {'max':'1070px'},
+    }
   }
   },
   plugins: [],
