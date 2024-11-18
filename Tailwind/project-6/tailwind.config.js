e57b076098f9117f50797ee2b48b1d45fab590ec
Muhammad Shakeeb Raza <shakeebraza90@gmail.com>
2024-11-17 23:57:43 -0800

    fix:"Creating Layers website in tailwind css"

--- Tailwind/project-6/tailwind.config.js
+++ Tailwind/project-6/tailwind.config.js
@@ -4,24 +4,13 @@ module.exports = {
   theme: {
 
     extend: {
-      colors :{
-        nilla: "#00baf2",
-        darkNilla: '#002970',
-        lightColor:'#f5f5f5',
-        lightNilla:'#00afe3',
-        lightDarkNilla:'#0f4a8a',
-        bgColor:'#f5f7fa',
-        ultalightNilla :'#4ccff6',
-        line:'#cccccc',
-        fontColor:'#505050',
-        smallFontColor:'#707086'
+     
+      screens :{
+        'extraSmall':{'max':'420px'},
+        'vsmall':{'max':'750px'},
+        'footer':{'max':'950px'},
+        'small':{'max':"1200px"}
     },
-    screens : {
-        'verySmall' : {'max' : "399px"},
-        'small' : {'max' :"550px"},
-        'medium' :{'max' :"768px"},
-        'large': {'max':'1070px'},
-    }
   }
   },
   plugins: [],
