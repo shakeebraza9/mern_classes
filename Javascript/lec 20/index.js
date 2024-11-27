7b534d5b6a6a3711993637a49a0d3eef09b8a3c6
Muhammad Shakeeb Raza <shakeebraza90@gmail.com>
2024-11-27 15:59:32 -0800

    fix:"javascript variable"

--- Javascript/lec 20/index.js
+++ Javascript/lec 20/index.js
@@ -4,3 +4,28 @@ console.log(a);
 
 name = "shakeeb";
 console.log(name);
+
+boolean = true;
+console.log(boolean)
+
+
+// is ko re declear kr sakty ho age ko
+var age = 20; // declear and value asigin both
+var age =30; // also declear and value asigin both
+console.log(age);
+
+// // is ko re declear nhi kr sakty ho age ko
+let age2; // declear
+age2 = 30 ; // asigin value
+let agenew = 40 // declear and value asigin both 
+console.log(age2,agenew)
+
+
+// na declear kr sakty ho or na dubara assign kr sakty ho 
+const cnicnumber = 41126789870000;
+console.log(cnicnumber);
+
+{
+    const cnicnumber = "chnage";
+    console.log(cnicnumber);  
+}
