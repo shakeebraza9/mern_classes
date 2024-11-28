4783ce85178ec7de4e42626f4eaef7cc62026475
Muhammad Shakeeb Raza <shakeebraza90@gmail.com>
2024-11-27 16:53:09 -0800

    fix:"operator in javascript"

--- Javascript/lec 21/index.js
+++ Javascript/lec 21/index.js
@@ -21,3 +21,61 @@ console.log(typeof sym);
 let bignumber = BigInt(371961289371892638912739812983); //BigInt
 console.log(typeof bignumber);
 
+// object Data type
+let stdInfo ={
+    name:"shakeeb",
+    roolenumber:123
+};
+console.log(stdInfo);
+console.log(typeof stdInfo);
+
+
+// arithmatic operator 
+
+// let sum = 4 + 7 ;
+// console.log(sum)
+
+// const num1 = 5 ;
+// const num2  = 20 ;
+
+// let multi = num1 * num2 ;
+// console.log(multi)
+
+
+// let div  = 3 % 2 ;
+// console.log(div)
+
+// let pow = 2 ** 5;
+// console.log(pow)
+
+
+// let channel = 'codeThread'
+
+// let val = 45 ;
+
+
+// let val = 4;
+//  val /= 2 ; // val = val / 2 
+// console.log(val)
+
+
+// let count = 4 ;
+// console.log("first count",--count)
+// console.log("second count",count)
+
+// console.log(2 && 0)
+// console.log(0 && -2)
+// console.log(2 || 0)
+// console.log(0 || 2)
+
+
+
+// let com = 4 !== '4' ;
+// console.log(com)
+
+// let sum = '1' + 1 + 1  ;
+// console.log( sum);
+
+// let b;
+// console.log( typeof(typeof(b)))
+
