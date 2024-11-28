dec4fb4e76a5c899f9d3ab01af5d7da310bfda45
Muhammad Shakeeb Raza <shakeebraza90@gmail.com>
2024-11-27 16:39:09 -0800

    fix:"Data type premitive datatype"

---
+++ Javascript/lec 21/index.js
@@ -0,0 +1,23 @@
+let num =20; // number
+console.log(typeof(num));
+
+let str ="hello world"; // string
+console.log(typeof str);
+
+let bool = true; // boolean
+console.log(typeof bool);
+
+let a = null; // null
+console.log(typeof a);
+
+let b ; // undefine 
+console.log(typeof b);
+
+//console.log(typeof c); // defined
+
+let sym = Symbol("Shakeebraza"); //Symbol
+console.log(typeof sym);
+
+let bignumber = BigInt(371961289371892638912739812983); //BigInt
+console.log(typeof bignumber);
+
