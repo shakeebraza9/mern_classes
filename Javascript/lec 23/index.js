44c2f943e3f246276d1559335af13da91e80cf56
Muhammad Shakeeb Raza <shakeebraza90@gmail.com>
2024-12-10 09:14:40 -0800

    fix:"function exp first"

--- Javascript/lec 23/index.js
+++ Javascript/lec 23/index.js
@@ -1,7 +1,18 @@
-const hindi = 50;
-const english = 50;
-const math = 50;
+// const hindi = 50;
+// const english = 70;
+// const math = 80;
 
-let total = math+english+hindi;
+// let total = math+english+hindi;
 
-console.log("ttal number "+total);
+// console.log("ttal number ",total);
+
+// function nameprint(){
+//     console.log("shakeeb raza ");
+// }
+// nameprint();
+
+function totalsum(name,math,english,hindi){
+    total = math+english+hindi;
+    console.log("hello ",name," your total number is ",total);
+}
+totalsum('shakeeb',80,60,100);
