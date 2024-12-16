66dcbf2f4539433d00d4edba39852db70b78bf1c
Muhammad Shakeeb Raza <shakeebraza90@gmail.com>
2024-12-16 13:42:00 -0800

    fix:"javascript "

--- Javascript/lec 23/index.js
+++ Javascript/lec 23/index.js
@@ -11,8 +11,66 @@
 // }
 // nameprint();
 
+// function totalsum(name,math,english,hindi){
+//    let total = math+english+hindi;
+//     console.log("hello ",name," your total number is ",total);
+// }
+// totalsum('shakeeb',80,60,100);
+// totalsum('aliz',60,70,100);
 function totalsum(name,math,english,hindi){
-    total = math+english+hindi;
-    console.log("hello ",name," your total number is ",total);
+   let total = math+english+hindi;
+    let msg = "hello "+ name +" your total number is "+total;
+    return msg;
 }
-totalsum('shakeeb',80,60,100);
+console.log(totalsum('shakeeb',80,60,100));
+
+
+// let num = 200.5634;
+// console.log(num.toFixed(2))
+// console.log(num.toPrecision(5)
+
+
+// let number = new Number(100)
+// console.log(number.toPrecision(4))
+
+
+// let str = "Heelo"
+// console.log(str)
+
+// let str = new String("Heelo")
+
+
+let str = "Code Thread"
+// console.log(str.replace(" ","EEE"))
+// console.log(str.split("e"))
+// console.log(str.length)
+// console.log(str.toLowerCase())
+// console.log(str.toUpperCase())
+// console.log(str.match("Code"))
+// console.log(str.charAt(5))
+// console.log(str.indexOf("Thread"))
+
+// console.log(Math)
+
+// console.log(Math.floor(3.99999))
+// console.log(Math.ceil(3.99999))
+// console.log(Math.max(3,35,6))
+// console.log(Math.min(3,35,6))
+// console.log(Math.round(6.5))
+// console.log(Math.abs(-5))
+// console.log(Math.random())
+
+
+// let randomNumber = Math.floor(Math.random() * 10 ) + 1; 
+
+// let min = 10 ;
+// let max = 20;
+// let randomNumber = Math.floor(Math.random() * (max- min + 1) )+ min;
+// console.log(randomNumber);
+
+
+let date = new Date();
+
+// console.log(Math.round(date.getTime()/1000))
+// console.log(date.toISOString())
+// console.log(date.toLocaleString())
