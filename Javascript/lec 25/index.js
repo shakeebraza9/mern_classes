180e7f868e926a074360eaaf69d0239177eec8a2
Muhammad Shakeeb Raza <shakeebraza90@gmail.com>
2024-12-30 15:09:15 -0800

    fix:"arrow function"

--- Javascript/lec 25/index.js
+++ Javascript/lec 25/index.js
@@ -1,29 +1,29 @@
-function sum (a,...num){ //rest operator
-  console.log(num)
-  let total = a+num[0]+num[1];
-  console.log(total)
-}
+// function sum (a,...num){ //rest operator
+//   console.log(num)
+//   let total = a+num[0]+num[1];
+//   console.log(total)
+// }
 
-sum(5,6,7);
+// sum(5,6,7);
 
 
-let obj ={
-  title:"lecture 25",
-  tags:"lec30,rest,world,shakeeb",
-  dsc:"hello world"
-}
+// let obj ={
+//   title:"lecture 25",
+//   tags:"lec30,rest,world,shakeeb",
+//   dsc:"hello world"
+// }
 
-let {title,...remaning}=obj;
-console.log(remaning)
+// let {title,...remaning}=obj;
+// console.log(remaning)
 
 // function obj1(...hello){
-function obj1({title,tags,dsc}){
-  let tagsarray=tags.split(",")
-  console.log(tagsarray)
+// function obj1({title,tags,dsc}){
+//   let tagsarray=tags.split(",")
+//   console.log(tagsarray)
   
-}
+// }
 // obj1(obj.title,obj.tags,obj.dsc); call like this function obj1(title,tags,dsc)
-obj1(obj); //call like this function obj1({title,tags,dsc})
+// obj1(obj); //call like this function obj1({title,tags,dsc})
 
 
 // let arr = [4,5,67,42,5,334,345]
@@ -44,11 +44,14 @@ let arr = [4,5,67,42,5,334,345];
 
 // }
 // arr.forEach(print);
-arr.forEach((val)=>console.log(val));
-let newarrayre=arr.map(function(val){
-  console.log(val);
-  return val;
-});
+// arr.forEach((val)=>console.log(val));
+
+// let newarrayre=arr.map(function(val){
+//   val+=5;
+//   return val;
+// });|
+
+let newarrayre=arr.map((val)=>val+=5);
 console.log(newarrayre);
 
 // arrow fun
