58aeacc80ada953abd2f50e01f0c8905f48a5abe
Muhammad Shakeeb Raza <shakeebraza90@gmail.com>
2025-02-01 12:41:07 -0500

    fix:"canhes can be done"

---
+++ Javascript/lec 30/index.js
@@ -0,0 +1,17 @@
+
+// let a =2 ;
+// let b ="2";
+// let c = a + b;
+// let d = a+Number(b);
+// console.log(typeof c);
+// console.log(typeof d);
+
+// console.log(3 + "45");
+// console.log(7+null);
+// console.log(7+true);
+// console.log(7-"8");
+// console.log("hello"-7);
+// console.log({}-7);
+// console.log([]-7);
+
+console.log(5-undefined)
