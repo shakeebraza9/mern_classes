5bed0d1c297d1a7e924fc595dc11d095227dafef
Muhammad Shakeeb Raza <shakeebraza90@gmail.com>
2025-02-03 05:41:29 -0500

    fix:"COERCION"

--- Javascript/lec 30/index.js
+++ Javascript/lec 30/index.js
@@ -1,17 +1,46 @@
+// console.log("kaise ho dost ")
 
-// let a =2 ;
-// let b ="2";
+// let a = 2 ;
+// let b = Number("4");
 // let c = a + b;
-// let d = a+Number(b);
-// console.log(typeof c);
-// console.log(typeof d);
+// console.log(c)
 
-// console.log(3 + "45");
-// console.log(7+null);
-// console.log(7+true);
-// console.log(7-"8");
-// console.log("hello"-7);
-// console.log({}-7);
-// console.log([]-7);
+// console.log( "45" + 9  + 3   )
 
-console.log(5-undefined)
+// console.log(7  + null)
+// console.log("7" + null)
+// console.log(4 + true)
+// console.log(5 - "56")
+// console.log("heelo" - 4)
+// console.log({} - 5 )
+// console.log([] - 6)
+
+// console.log(5 - undefined)
+
+// let obj = {
+//     a: 45,
+//     toString : function () {
+//         return "Helloji 3";
+//     },
+//     valueOf: function () {
+//         return 45;
+//     }
+// };
+// let obj = {a: 45, toString() {return "Helloji"}, valueOf(){return 50}, valueOf(){return 10}};
+
+// console.log(obj + " Dosto");
+// obj -> valueOf() - >return object ->toString() -> return
+//  [object Object] and this is string
+
+// console.log(5 - obj)
+
+
+// let obj2 = {"b":5 , valueOf() {return 60} , toString() {return "hello"}}
+
+// console.log(5 + `${obj2}`) // hind - "string"
+// console.log(typeof `${obj2}`)
+
+// let obj2 = {"b":5 , valueOf() {return {}} , toString() {return {}}}
+//error case
+
+// console.log(2 + obj2)
