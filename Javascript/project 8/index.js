9613b2d288d41acc12f34ebabebbcfe22b162d72
Muhammad Shakeeb Raza <shakeebraza90@gmail.com>
2025-03-30 07:17:14 -0500

    fix:"PROJECT 10 | Making Tic Tac Toe Game in JavaScript "

--- Javascript/project 8/index.js
+++ Javascript/project 8/index.js
@@ -1,16 +1,54 @@
 const tictac = document.querySelector('.tictac');
+const boxes = document.querySelectorAll('.box');
+const h1 = document.getElementsByTagName("h1");
+let winingCondition = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
 let currentPlayer = "X"
-tictac.addEventListener('click',(e)=>{
-   if(e.target.className !== "tictac"){
-    if(e.target.innerText ===""){
-        e.target.textContent = currentPlayer;
-        currentPlayer = (currentPlayer === "X") ? "O" : "X"
+function startgame(e){
+    if(e.target.className !== "tictac"){
+        if(e.target.innerText ===""){
+            e.target.textContent = currentPlayer;
+            winner();
+            currentPlayer = (currentPlayer === "X") ? "O" : "X"
         }
-   }
+    }
+    
 
+}
 
+tictac.addEventListener('click',startgame)
+function winner(){
+    // console.log(boxes[0].innerHTML)
+    winingCondition.forEach((item)=>{
+      let index0 = item[0]
+      let index1 = item[1]
+      let index2 = item[2]
+    //   console.log(item, index0)
 
-})
+    let val0 = boxes[index0].innerText;
+    let val1 = boxes[index1].innerText;
+    let val2 = boxes[index2].innerText;
+    console.log(item[0] , val0 , item[1] , val1 , item[2] , val2 )
 
+    if(val0 !== "" && val1 !== "" && val2 !== ""){
+        if (val0 === val1 && val0 === val2) {
+            console.log("final check")
+            boxes[item[0]].classList.add("winnerClass")
+            boxes[item[1]].classList.add("winnerClass")
+            boxes[item[2]].classList.add("winnerClass")
+            count = 0
+            h1[0].innerText = `Winner is ${val1}`;
+            tictac.removeEventListener("click", startgame);
+        }
+    }
+    })
+}
 
-const box = document.querySelector('.box');
