6b63ffd0ef05e7b8ec8bca568f708799c2456d5e
shakeeb raza <shakeebraza90@gmail.com>
2025-04-02 15:19:29 +0500

    fix:"closure"

--- Javascript/lec 34/index.js
+++ Javascript/lec 34/index.js
@@ -1,12 +1,28 @@
-function hello(text, para) {
-    console.log("inside hello");
-    // console.log(para);
-    para(text);
-}
+// function hello(text, para) {
+//     console.log("inside hello");
+//     // console.log(para);
+//     para(text);
+// }
 
 // function cb() {
 //     console.log("inside callback");
 // }
-hello("hello world", (data) => {
-    console.log("inside callback", data);
-});
+// hello("hello world", (data) => {
+//     console.log("inside callback", data);
+// });
+
+function fun1(text) {
+    let a = 50;
+
+    function fun2() {
+        console.log(text);
+        console.log(a);
+    }
+    a = 60;
+    // fun2();
+
+    return fun2;
+}
+
+var res = fun1("hello world");
+res();
