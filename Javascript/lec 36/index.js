36859c5bfbd8d6a5f0a0ffe7378942a9837d0e82
Muhammad Shakeeb Raza <shakeebraza90@gmail.com>
2025-04-20 08:21:01 -0500

    Promise in JavaScript

--- Javascript/lec 36/index.js
+++ Javascript/lec 36/index.js
@@ -1,18 +1,45 @@
-const promise1 = new Promise((resolve, reject) => {
-    console.log("inside promise");
-    resolve("resolve promise ")
-    // reject("reject promise")
-}).then(
-    (result) => {
-        console.log(result)
-    },
-    function reject(err) {
-        console.log(err)
-    }
-)
+// const promise1 = new Promise((resolve, reject) => {
+//     console.log("inside promise");
+//     resolve("resolve promise ")
+//     // reject("reject promise")
+// }).then(
+//     (result) => {
+//         console.log(result)
+//     },
+//     function reject(err) {
+//         console.log(err)
+//     }
+// )
 
 // promise1.then((resolve) => {
 //     console.log(resolve)
 // }, function reject(err) {
 //     console.log(err)
 // })
+
+
+function isData(item) {
+    const promise1 = new Promise(function fun1(resolve, reject) {
+        if (item) {
+            resolve("promise resolve");
+        } else {
+            reject("dataa is empty");
+        }
+    });
+
+    return promise1;
+}
+
+let result = isData("rbgdfgc");
+let x = result
+    .then(function success(result) {
+        console.log(result);
+        return "heelllo from .then()";
+    })
+    .then(function secondFun(e) {
+        console.log(e);
+        return "nishant";
+    })
+    .then(function nish(data) {
+        console.log("data", data);
+    });
