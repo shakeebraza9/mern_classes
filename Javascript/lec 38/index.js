bd27cfaaeee856c6c6f5abbcd1f5c207e4fba6c3
Muhammad Shakeeb Raza <shakeebraza90@gmail.com>
2025-10-17 14:03:57 +0500

    Async Await in JavaScript

--- Javascript/lec 38/index.js
+++ Javascript/lec 38/index.js
@@ -1,23 +1,43 @@
-function fun1(){
-    return 'hello' ;
+// function fun1(){
+//     return 'hello' ;
+// }
+// let retf = fun1();
+// console.log(retf);
+
+// async function fun2(){
+//     return 'hello' ;
+// }
+// fun2().then((data)=>{
+// console.log(data)
+// })
+// // let ret = fun2();
+// // console.log(ret);
+// // Async functions return promise
+
+// function fun3(){
+//     return Promise.resolve("hello")
+//     // return new Promise(function(res,rej){
+//     //     res("hello")
+//     // }) 
+// }
+// fun3().then((data)=>{
+// console.log(data)
+// })
+// let res3 = fun3();
+// console.log(res3)
+
+
+function pro(){
+    return new Promise(function pro1(res,rej){
+        res("hello");
+    })
 }
-let retf = fun1();
-console.log(retf);
 
 async function fun2(){
-    return 'hello' ;
+    let x = await pro();
+    console.log(x);
+    return 'M Shakeeb raza' ;
 }
-
-let ret = fun2();
-console.log(ret);
-// Async functions return promise
-
-function fun3(){
-    // return Promise.resolve("hello")
-    return new Promise(function(res,rej){
-        res("hello")
-    }) 
-}
-
-let res3 = fun3();
-console.log(res3)
+fun2().then((data)=>{
+console.log(data)
+})
