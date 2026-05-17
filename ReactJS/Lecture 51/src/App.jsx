9efe968885857c033e84a380f6430a0c668f686b
Muhammad Shakeeb Raza <shakeebraza90@gmail.com>
2026-05-17 17:04:03 +0500

    What is Hooks ? | useState hook

--- ReactJS/Lecture 51/src/App.jsx
+++ ReactJS/Lecture 51/src/App.jsx
@@ -1,28 +1,31 @@
 import { useState } from "react";
 
   function App() {
-    let count = 0;
-    let [data,setData] = useState(2);
-    console.log(data);
-    function incressval(){
-      count = count + 1;
+    // let count = 0;
 
-    }
+    let [data,setData] = useState(0);
+
+    // console.log(data);
+
     function incressval(){
-      count = count + 1;
-      console.log(count);
+      // setData(data + 1);
+      setData((data) => data + 1);
+      // count = count + 1;
+      // console.log(count);
 
     }
     function Drecessval(){
-      count = count - 1;
-      console.log(count);
+      // setData(data - 1);
+      setData((data) => data - 1);
+      // count = count - 1;
+      // console.log(count);
 
     }
   return(
     <div>
       <h1>Hello, World!</h1>
       <button onClick={incressval}>Increment</button>
-      <h2>{count}</h2>
+      <h2>{data}</h2>
       <button onClick={Drecessval}>Decrment</button>
     </div>
   )
