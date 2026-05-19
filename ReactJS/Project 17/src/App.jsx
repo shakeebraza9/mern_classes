a848d323d27b309a890d8dbff1a3f63b28899e82
Muhammad Shakeeb Raza <shakeebraza90@gmail.com>
2026-05-19 18:19:25 +0500

    PROJECT 17 | Basic React Project

--- ReactJS/Project 17/src/App.jsx
+++ ReactJS/Project 17/src/App.jsx
@@ -4,15 +4,20 @@ import AllCards from "./component/AllCards";
 import {data} from "./utilis/data.js";
   
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredData, setFilteredData] = useState(data);
+  
   function filterHandler(){
-    console.log("filter handler called")
+    let res = data.filter((item) => item.title.toLowerCase().includes(searchTerm.toLowerCase()))
+    setFilteredData(res)
+  
   }
   return(
     <div>
       <Heading />
-      <input type="text" placeholder="Kya Cheya..." />
+      <input type="text" placeholder="Kya Cheya..."  onChange={(e) => setSearchTerm(e.target.value)} />
       <button onClick={filterHandler}>Search</button>
-      <AllCards data={data} />
+      <AllCards data={filteredData} />
     </div>
   )
     
