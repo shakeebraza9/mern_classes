fe89bc51b17c3d13db94a18e5a6e07df674a0141
Muhammad Shakeeb Raza <shakeebraza90@gmail.com>
2026-05-20 15:35:17 +0500

    React Js Form Handling & useRef Hook

--- ReactJS/Lecture 54 - useRef Hook/src/App.jsx
+++ ReactJS/Lecture 54 - useRef Hook/src/App.jsx
@@ -3,15 +3,31 @@ import { useState } from "react";
   
 function App() {
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function fromSubmite(e){
+    e.preventDefault();
+    console.log({ email, password });
+  }
+
+  function handleEmail(event){
+    setEmail(event.target.value)
+  }
+
+  function handlePassword(event){
+    setPassword(event.target.value)
+  }
+
   return(
     <div>
       <form action="">
-        <input type="text" placeholder="Email" />
+        <input type="text" placeholder="Email" onChange={handleEmail} />
         <br />
-        <input type="password" placeholder="password" />
+        <input type="password" placeholder="password" onChange={handlePassword}/>
         <br />
         <br />
-        <button type="submit">Submit</button>
+        <button onClick={fromSubmite}>Submit</button>
       </form>
     </div>
   )
