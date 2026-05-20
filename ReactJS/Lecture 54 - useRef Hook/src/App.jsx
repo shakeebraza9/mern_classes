8b20419202e7ba4cc7d6c6c5903ef3eef99d2cb1
Muhammad Shakeeb Raza <shakeebraza90@gmail.com>
2026-05-20 14:54:30 +0500

    React Js Form Handling & useRef Hook

---
+++ ReactJS/Lecture 54 - useRef Hook/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+  
+function App() {
+
+  return(
+    <div>
+      <form action="">
+        <input type="text" placeholder="Email" />
+        <br />
+        <input type="password" placeholder="password" />
+        <br />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
+    
+
+  }
+
+  export default App
