076a45f717c801b0031301f96a4b18048460d682
Muhammad Shakeeb Raza <shakeebraza90@gmail.com>
2026-05-20 15:57:50 +0500

    React Js Form Handling & useRef Hook

--- ReactJS/Lecture 54 - useRef Hook/src/Uncontrolled.jsx
+++ ReactJS/Lecture 54 - useRef Hook/src/Uncontrolled.jsx
@@ -3,9 +3,11 @@ import { useRef } from "react";
 function Uncontrolled() {
     let emailRef = useRef();
     let passRef = useRef();
+    let h1ref = useRef();
 
     // emailRef.current.style.backgroundColor = "cyan";
     // passRef.current.style.backgroundColor = "cyan";
+    h1ref.current.innerText = "Uncontrolled Component by SHakeeb";
 
     function formSubmit(event) {
         event.preventDefault();
@@ -17,7 +19,7 @@ function Uncontrolled() {
 
     return (
         <>
-            <h1>Uncontrolled</h1>
+            <h1 ref={h1ref}> Uncontrolled</h1>
             <form>
                 <input type="text" placeholder="email" ref={emailRef} />
                 <br />
